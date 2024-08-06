deafda00a0cb596cf2a69be7bdce5e9ec38cb5c2
Toan Phung <toan.phung@proteo.info>
2024-08-06 14:03:40 +0100

    Update settings.py

--- catapult_backend/settings.py
+++ catapult_backend/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e*%$y7n7#+%-te73v&pf#iwh66q$wqvgc_&a^_x-x!-7gz0c=d'
+SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-e*%$y7n7#+%-te73v&pf#iwh66q$wqvgc_&a^_x-x!-7gz0c=d')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # Application definition
 
