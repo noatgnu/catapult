5b5e94ab822822ee9e6380489e9ea90f79079f18
Toan Phung <toan.phung@proteo.info>
2024-08-06 14:55:06 +0100

    Update middleware.py

--- catapult/middleware.py
+++ catapult/middleware.py
@@ -8,24 +8,24 @@ class DemoModeMiddleware:
     def __call__(self, request):
         if settings.DEMO:
             restricted_endpoints = [
-                '/api/api-keys/',
-                '/api/file-browser/',
-                '/admin/',
-                '/api/experiments/',
-                '/api/analyses/',
-                '/api/files/',
-                '/api/uploadedfiles/',
-                '/api/folderlocations/',
-                '/api/tasks/',
-                '/api/workers/',
-                '/api/logrecords/',
-                '/api/proteingroup/',
-                '/api/precursor/'
+                '/api/api-keys',
+                '/api/file-browser',
+                '/admin',
+                '/api/experiments',
+                '/api/analyses',
+                '/api/files',
+                '/api/uploadedfiles',
+                '/api/folderlocations',
+                '/api/tasks',
+                '/api/workers',
+                '/api/logrecords',
+                '/api/proteingroup',
+                '/api/precursor'
                 # Add more endpoints as needed
             ]
             print(request.path)
-            if request.path in restricted_endpoints:
-                if request.method != 'GET':
+            for endpoint in restricted_endpoints:
+                if request.path.startswith(endpoint) and request.method != 'GET':
                     return JsonResponse({'error': 'This action is not allowed in demo mode.'}, status=403)
         response = self.get_response(request)
         return response
